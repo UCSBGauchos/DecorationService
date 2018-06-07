f83591a041bd61f4189dca5a850ffbc45e0fa612
Bo Yang <boyan2@cisco.com>
2018-06-06 17:19:11 -0700

    update vendor

--- tests/default_test.go
+++ tests/default_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 	"runtime"
 	"path/filepath"
-	_ "wwwin-github.cisco.com/DevNet/DecorationService/routers"
+	_ "github.com/UCSBGauchos/DecorationService/routers"
 
 	"github.com/astaxie/beego"
 	. "github.com/smartystreets/goconvey/convey"
